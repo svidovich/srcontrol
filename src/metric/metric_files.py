5fbbc0f978979af84cc1d1934f03e5f2668c98ce
mydriasis <samuel.vidovich@gmail.com>
2019-05-01 23:16:07 -0400

    Separate data

--- src/metric/metric_files.py
+++ src/metric/metric_files.py
@@ -1,19 +1,26 @@
 # TODO: Add data for all of the various possible hierarchies that
 # allows us to detect which files are settings and which are metrics
 
-cpu_cpuaccount_metrics_files = [
-    'cpuacct.usage',
-    'cpuacct.usage_percpu_user',
+general_metrics_files = [
     'cgroup.procs',
+    'cgroup.sane_behavior'
+    'tasks',
+]
+
+cpu_metrics_files = [
+    'cpu.cfs_period_us'
     'cpu.shares',
+    'cpu.stat',
+]
+
+cpuaccount_metrics_files = [
+    'cpuacct.stat',
     'cpuacct.usage_all',
+    'cpuacct.usage_percpu_sys'
+    'cpuacct.usage_percpu_user',
+    'cpuacct.usage_percpu',
     'cpuacct.usage_sys',
     'cpuacct.usage_user',
-    'tasks',
-    'cgroup.sane_behavior',
-    'cpu.stat',
-    'cpuacct.usage_percpu',
-    'cpu.cfs_period_us',
-    'cpuacct.stat',
-    'cpuacct.usage_percpu_sys'
+    'cpuacct.usage',
 ]
+
